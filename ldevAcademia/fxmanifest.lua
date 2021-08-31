659e56bbb1f84f6932851b7a49bb5546e86d3124
Felipe Roberti Amaral <77501866+flproberti@users.noreply.github.com>
2021-08-31 15:11:46 -0300

    Add files via upload

---
+++ ldevAcademia/fxmanifest.lua
@@ -0,0 +1,18 @@
+--[ Infos ]--
+fx_version 'bodacious'
+game 'gta5'
+author 'flproberti#2006'
+contact 'Discord: discord.gg/ngPhYAeFsP'
+
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'ldevClient.lua',
+	'ldevConfig.lua',
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'ldevServer.lua',
+	'ldevConfig.lua',
+}
